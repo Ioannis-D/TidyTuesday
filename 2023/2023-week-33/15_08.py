c9022c3639bd79468077489d4ab4c96460f19347
Ioannis Doganos <i.doganos@tutamail.com>
2023-08-20 16:49:59 +0200

    Week 33

--- 2023/2023-week-33/15_08.py
+++ 2023/2023-week-33/15_08.py
@@ -1,11 +1,14 @@
+# TidyTuesday, week 33 of 2023, creator: Ioannis Doganos
+
 # Import the libraries
 import pandas as pd
 import numpy as np
 from math import pi
 
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 
-# Load custom fonts
+# Load custom fonts (not all fonts are used)
 import matplotlib as mpl
 from pathlib import Path
 
@@ -14,6 +17,7 @@ alfaslabpath = Path(mpl.get_data_path(), "./fonts/ttf/AlfaSlabOne-Regular.ttf")
 handjetpath = Path(mpl.get_data_path(), "./fonts/ttf/Handjet.ttf")
 handjetlightpath = Path(mpl.get_data_path(), "./fonts/ttf/Handjet-Light.ttf")
 
+# ---- DATA ---- 
 # Read the data
 spam = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-08-15/spam.csv')
 
@@ -23,7 +27,8 @@ for column in spam.columns[1:-1]:
     percentages[column] = round((spam[spam[column] > 0].groupby('yesno')[column].count() / spam.groupby('yesno')[column].count())*100, 2)
 
 
-#---- PLOT ----
+# ---- PLOT ----
+
 # The colors
 green_general = '#00f275'
 green_fill = '#26ff8f'
@@ -96,6 +101,11 @@ for label, angle in zip(ax1.get_xticklabels(), angles):
 
 # Ensure radar goes from 0 to 100.
 ax1.set_ylim(0, 100)
+# Change the tick labels in order to show as percentage
+# First set the ticks because of a bug in matplotlib that shows a warning message
+# Solution found: https://stackoverflow.com/questions/63723514/userwarning-fixedformatter-should-only-be-used-together-with-fixedlocator
+ax1.set_yticks(list(range(20, 120, 20)))
+ax1.set_yticklabels(['20%', '40%', '60%', '80%', ''])
 
 # Set position of y-labels (0-100) to be in the middle
 # of the first two axes.
@@ -105,7 +115,7 @@ ax1.set_rlabel_position(180 / num_vars)
 # Change the color and the size of the tick labels.
 ax1.tick_params(colors=labels_color, labelsize = 30)
 # Make the y-axis (0-100) labels smaller.
-ax1.tick_params(axis='y', labelsize=10)
+ax1.tick_params(axis='y', labelsize = 18)
 # Change the color of the circular gridlines.
 ax1.grid(color='#555555', alpha = 0.6)
 # Change the color of the outermost gridline (the spine).
@@ -118,19 +128,18 @@ fig1.patch.set_facecolor(background_color)
 # Lastly, give the chart a title
 ax1.set_title('Presence of symbols/words', y=1.1, color = labels_color, size = 30)
 
-# Save the plot as an image
+# Save the plot as image
 plt.savefig('RadarChart.png')
 
 
-# ---- The boxplots ----
+# ---- Boxplots ----
 
 spam_email = spam[(spam['yesno'] == 'y') & (spam['crl.tot'] < 5000)]
 no_spam_email = spam[(spam['yesno'] == 'n') & (spam['crl.tot'] < 5000)]
 
-
 fig2, (ax2, ax3) = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(12, 12))
 
-# ----The boxplot for no-spam----
+# ----Boxplot for no-spam----
 # The attributes for the coloring of the boxplot
 boxprops = dict(color=green_general, facecolor=green_fill, linewidth=1, alpha = 0.8)
 flierprops = dict(marker='o', markersize=6, markeredgecolor=green_outliers)
@@ -153,11 +162,11 @@ ax2.spines[['top', 'right', 'bottom', 'left']].set_visible(False)
 # Change the color of the labels
 ax2.tick_params(axis='y', labelcolor=labels_color)
 # Change the fontsize of the ticks
-plt.rc('ytick',  labelsize = 15)
+plt.rc('ytick',  labelsize = 19)
 # Change the background color
 ax2.set_facecolor(background_color)
 
-# ----The boxplot for spam----
+# ----Boxplot for spam----
 # The attributes for the coloring of the boxplot
 boxprops = dict(color=red_general, facecolor=red_fill, linewidth=1, alpha = 0.8)
 flierprops = dict(marker='o', markersize=6, markeredgecolor=red_outliers)
@@ -186,7 +195,7 @@ ax3_position = ax3.get_position()
 ax2_position = ax2.get_position()
 ax3.set_position([ax2_position.x1, ax3_position.y0, ax3_position.width, ax3_position.height])
 
-# Change the figure's background to mach the plots' background
+# Change the figure's background to mach the plot's background
 fig2.patch.set_facecolor(background_color)
 
 # Put a title
@@ -196,16 +205,15 @@ plt.title('Number of characters', color = labels_color, fontsize = 30, x = 0.055
 plt.savefig('Boxplots.png')
 
 
-# ---- 'TITLE' and 'SUBTITLE' ----
-fig3, ax4 = plt.subplots(figsize = (12,3.5))
+# ---- Title and Subtitle ----
+fig3, ax4 = plt.subplots(figsize = (12, 4.5))
 
-ax4.text(0.5, 0.9, 'Characteristics of', fontsize = 30, color = labels_color, ha = 'center', font = arbilpath)
-ax4.text(0.44, 0.41, 'NO ', fontsize = 100, color = green_general, ha = 'right', font = alfaslabpath)
-ax4.text(0.44, 0.41, '$PAM',fontsize = 100, color = red_general, ha = 'left', font = alfaslabpath)
-ax4.text(0.5, 0.25, 'e-mails', fontsize = 30, color = labels_color, ha = 'center', font = arbilpath)
+ax4.text(0.5, 0.9, 'Characteristics of', fontsize = 30, color = labels_color, ha = 'center', font = alfaslabpath)
+ax4.text(0.44, 0.4, 'NO ', fontsize = 100, color = green_general, ha = 'right', font = alfaslabpath)
+ax4.text(0.44, 0.4, '$PAM',fontsize = 100, color = red_general, ha = 'left', font = alfaslabpath)
 ax4.text(0.5, 0.08, '''Spam emails tend to be  l o n g e r,
-mention more the term "make money" or include symbols like the ! and the $''',
-        fontsize = 20, color = labels_color, ha = 'center', va = 'center', font = handjetlightpath)
+mention more the term "make money" or include special characters like the ! and the $''',
+        fontsize = 25, color = labels_color, ha = 'center', va = 'center', font = handjetlightpath)
 # Remove the axis
 ax4.xaxis.set_visible(False)
 ax4.yaxis.set_visible(False)
@@ -213,13 +221,12 @@ ax4.yaxis.set_visible(False)
 ax4.spines[['top', 'right', 'bottom', 'left']].set_visible(False)
 # Change the background color
 ax4.set_facecolor(background_color)
-
 fig3.patch.set_facecolor(background_color)
 
-# Save the plot as animage
+# Save as image
 plt.savefig('Title.png')
 
-# ---- MERGING THE IMAGES ----
+# ---- Merging the images ----
 
 from PIL import Image
 
@@ -237,13 +244,13 @@ radar_size = radar.resize((2000, 1800))
 # Create a new image and pasting the images
 img = Image.new("RGB", (4000, 2700), background_color)
 
-# Paste the 'title'
+# Paste the title
 img.paste(title_size, (0, 0))
 # Paste the boxplots
 img.paste(boxplots_size, (2300, 1000))
-#Paste the radar chart
+# Paste the radar chart
 img.paste(radar_size, (0, 1000))
 
-# Save and show the final image
+# Save and show the image
 img.show()
 img.save('15_08.png')
